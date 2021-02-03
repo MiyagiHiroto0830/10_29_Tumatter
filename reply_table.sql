5190d3de2896ca31b212feabcf523f806aa562ef
MiyagiHiroto0830 <hirotobaseball0830@icloud.com>
2021-02-03 12:40:12 +0900

    update

--- reply_table.sql
+++ reply_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost
--- 生成日時: 2021 年 1 月 28 日 14:30
+-- 生成日時: 2021 年 2 月 03 日 04:38
 -- サーバのバージョン： 10.4.17-MariaDB
 -- PHP のバージョン: 7.3.24
 
@@ -32,17 +32,29 @@ CREATE TABLE `reply_table` (
   `reply_comment` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
   `reply_id` int(11) NOT NULL,
   `reply_created_at` datetime NOT NULL,
-  `username` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
+  `reply_username` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reply_image` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- テーブルのデータのダンプ `reply_table`
 --
 
-INSERT INTO `reply_table` (`id`, `reply_comment`, `reply_id`, `reply_created_at`, `username`) VALUES
-(26, 'カメハメは打ちたい', 24, '2021-01-28 21:26:16', '太郎先生'),
-(28, 'すごい', 24, '2021-01-28 21:39:03', '2019211130'),
-(29, 'カメハメは打ちたい', 23, '2021-01-28 21:52:04', '2019211130');
+INSERT INTO `reply_table` (`id`, `reply_comment`, `reply_id`, `reply_created_at`, `reply_username`, `reply_image`) VALUES
+(34, 'すごい', 41, '2021-01-29 23:23:45', 'hirohiro', NULL),
+(36, 'コードなんて書かなくていいんですよ', 46, '2021-01-30 12:56:18', '太郎先生', NULL),
+(37, '頑張ろー', 47, '2021-01-30 12:58:27', '坂本勇人', NULL),
+(38, '楽しみー', 47, '2021-01-30 12:59:19', '柳田悠岐', NULL),
+(43, 'phpだーいすき', 47, '2021-01-31 20:24:02', '篠山紀信', NULL),
+(45, 'カメハメは打ちたい', 52, '2021-02-02 23:32:05', '篠山紀信', NULL),
+(46, '', 54, '2021-02-02 23:44:45', '篠山紀信', 'upload/20210202154445eeb28c5fc9409ba2d9e6d64f192ad8dc.jpg'),
+(47, '', 58, '2021-02-02 23:48:43', '篠山紀信', 'upload/202102021548434673278333889b8cf79e779c696b0fcf.jpg'),
+(48, 'カメハメは打ちたい', 57, '2021-02-02 23:54:16', '篠山紀信', 'upload/202102021554167c9595b88c78f4491864e938ab4bd678.jpeg'),
+(50, 'すごい', 52, '2021-02-03 01:21:11', '篠山紀信', 'upload/20210202172111506d88a68b2e3913823bae6e0ebd85ef.'),
+(51, 'すごい', 77, '2021-02-03 01:52:10', '篠山紀信', NULL),
+(52, 'pppp', 58, '2021-02-03 01:52:30', '篠山紀信', NULL),
+(53, 'カメハメは打ちたい', 58, '2021-02-03 01:52:54', '篠山紀信', NULL),
+(54, 'plpkp', 58, '2021-02-03 01:53:07', '篠山紀信', 'upload/202102021753079fb6f47aca49a614af55032c14650a70.jpg');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -62,7 +74,7 @@ ALTER TABLE `reply_table`
 -- テーブルの AUTO_INCREMENT `reply_table`
 --
 ALTER TABLE `reply_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- todo_table.sql
+++ todo_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost
--- 生成日時: 2021 年 1 月 28 日 14:30
+-- 生成日時: 2021 年 2 月 03 日 04:34
 -- サーバのバージョン： 10.4.17-MariaDB
 -- PHP のバージョン: 7.3.24
 
@@ -33,17 +33,30 @@ CREATE TABLE `todo_table` (
   `deadline` date NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  `username` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
+  `username` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- テーブルのデータのダンプ `todo_table`
 --
 
-INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated_at`, `username`) VALUES
-(23, 'php嫌だ', '0000-00-00', '2021-01-28 20:47:56', '2021-01-28 20:47:56', 'hirohiro'),
-(24, 'ooo', '0000-00-00', '2021-01-28 21:20:36', '2021-01-28 21:20:36', '2019211130'),
-(25, 'k', '0000-00-00', '2021-01-28 21:38:42', '2021-01-28 21:38:42', '太郎先生');
+INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `created_at`, `updated_at`, `username`, `image`) VALUES
+(46, 'phpわからん\r\ntumatter\r\n教えて', '0000-00-00', '2021-01-30 12:55:49', '2021-01-30 12:55:49', 'うっシー', NULL),
+(47, '日本に帰って来ました！！！！', '0000-00-00', '2021-01-30 12:57:36', '2021-01-30 12:57:36', '田中将大', NULL),
+(48, 'かかロットーーーー', '0000-00-00', '2021-01-30 13:06:06', '2021-01-30 13:06:06', '＠破壊王子ベジータ', NULL),
+(50, '00000000', '0000-00-00', '2021-01-30 13:31:25', '2021-01-30 13:31:25', '篠山紀信', NULL),
+(51, 'php6666', '0000-00-00', '2021-01-30 13:36:25', '2021-01-30 13:36:25', '篠山紀信', NULL),
+(52, 'pppp', '0000-00-00', '2021-01-31 20:23:39', '2021-01-31 20:23:39', '篠山紀信', NULL),
+(53, 'eeeee', '0000-00-00', '2021-02-02 20:10:12', '2021-02-02 20:10:12', '篠山紀信', 'upload/202102021210125e4bd511f71f2b63d77a27f87863e8e6.jpg'),
+(54, 'ww', '0000-00-00', '2021-02-02 21:33:03', '2021-02-02 21:33:03', '篠山紀信', 'upload/202102021333035e884e98aa6a87924624629a5ee5f394.jpeg'),
+(57, '', '0000-00-00', '2021-02-02 22:07:04', '2021-02-02 22:07:04', '篠山紀信', 'upload/2021020214070407b26258d31609866aae89518d1e41d7.jpg'),
+(58, '111111', '0000-00-00', '2021-02-02 22:33:55', '2021-02-02 22:33:55', '篠山紀信', 'upload/20210202143355aff53045a40c71d64e5dab3b2b5c97b8.jpeg'),
+(75, 'j', '0000-00-00', '2021-02-03 01:49:41', '2021-02-03 01:49:41', '篠山紀信', NULL),
+(76, 'lplplp', '0000-00-00', '2021-02-03 01:50:35', '2021-02-03 01:50:35', '篠山紀信', NULL),
+(77, 'lplppl', '0000-00-00', '2021-02-03 01:50:51', '2021-02-03 01:50:51', '篠山紀信', 'upload/20210202175051661592414d783c3f953f2791ad84140a.jpg'),
+(78, 'ppp', '0000-00-00', '2021-02-03 01:53:40', '2021-02-03 01:53:40', '篠山紀信', NULL),
+(79, 'qqw', '0000-00-00', '2021-02-03 12:31:35', '2021-02-03 12:31:35', '篠山紀信', NULL);
 
 --
 -- ダンプしたテーブルのインデックス
@@ -63,7 +76,7 @@ ALTER TABLE `todo_table`
 -- テーブルの AUTO_INCREMENT `todo_table`
 --
 ALTER TABLE `todo_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
